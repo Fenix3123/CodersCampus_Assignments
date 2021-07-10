3349d18dfdb061d9c105807ba8f2f8805738d415
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2021-07-10 09:51:01 -0500

    tried things

--- Assignment5/CustomArrayList.java
+++ Assignment5/CustomArrayList.java
@@ -1,13 +1,27 @@
 //package com.coderscampus.arraylist;
+import java.util.List;
+import java.util.ArrayList;
 
 public class CustomArrayList<T> implements CustomList<T> {
-	Object[] items = new Object[100];
+	Object[] items = new Object[10];
+	List<Object> items2 = new ArrayList<>();
 	private int i = 0;
+	
 	@Override
 	public boolean add(T item) {
 		// TODO Auto-generated method stub
+		
 		items[i] = item;
+		items2.add(item);
 		i++;
+		
+		
+		if(items.length == i) {
+			items = new Object[items.length*2];
+			for(int j = 0; j < items.length; j++) {
+				items[j] = items2.get(j);
+			}
+		}
 		
 		return true;
 	}
@@ -23,14 +37,14 @@ public class CustomArrayList<T> implements CustomList<T> {
 	public T get(int index) {
 		// TODO Auto-generated method stub
 		//int arrSize = items.length;
-		Object smth = null;
-		for(int i = 0; i < items.length; i++) {
-			if(items[index].equals(items[i])) {
-				smth = items[index];
-			}
+		//Object smth = null;
+		//for(int i = 0; i < items.length; i++) {
+		//	if(items[index].equals(items[i])) {
+		//		smth = items[index];
+		//	}
 			
-		}
-		return (T)smth;
+		//}
+		return (T)items[index];
 	}
 	
 }
--- Assignment5/CustomTest.java
+++ Assignment5/CustomTest.java
@@ -4,11 +4,18 @@ public class CustomTest {
 		
 		CustomList<String> myCustomList = new CustomArrayList<>();
 		myCustomList.add("element 1"); // and continue to add another 10, 20 or 40 more elements
-		myCustomList.add("element 2");
+		for(int i = 0; i < 8; i++) {
+			myCustomList.add("element 1"); 
+			
+		}
+		
+		myCustomList.add("element 1");
+		
 
 		// then you should validate that all the elements you've inserted actually exist in your data structure
 		for (int i=0; i<myCustomList.getSize(); i++) {
 		    System.out.println(myCustomList.get(i));
+		    
 		}
 
 	}

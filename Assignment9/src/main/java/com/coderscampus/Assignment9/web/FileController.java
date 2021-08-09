537fb814d1cb5ac66b8009cab9f199e4411b99a8
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2021-08-09 17:48:44 -0500

    update

--- Assignment9/src/main/java/com/coderscampus/Assignment9/web/FileController.java
+++ Assignment9/src/main/java/com/coderscampus/Assignment9/web/FileController.java
@@ -18,12 +18,75 @@ public class FileController {
 	private recipeMaker reader;
 	
 	@GetMapping("/all-recipes")
-	public void returnAll() throws IOException{
+	public List<Recipe> returnAll() throws IOException{
 		List<Recipe> recipes = reader.readFile();
 		
 		for(Recipe item: recipes) {
 			System.out.println(item);
 		}
+		
+		return recipes;
+		
+		
+	}
+	
+	@GetMapping("/vegan")
+	public List<Recipe> returnVegan() throws IOException{
+		//List<Recipe> recipes = reader.readFile();
+		List<Recipe> recipes_vegan = reader.readFile().stream()
+													  .filter(vegan -> vegan.getVegan() == true)
+													  .toList();
+		
+		for(Recipe item: recipes_vegan) {
+			System.out.println(item);
+		}
+		
+		return recipes_vegan;
+		
+	}
+	
+	@GetMapping("/vegan-and-gluten-free")
+	public List<Recipe> returnVeganAndGlutenFree() throws IOException{
+		//List<Recipe> recipes = reader.readFile();
+		List<Recipe> recipes_vg = reader.readFile().stream()
+													  .filter(item -> item.getVegan() == true && item.getGlutenFree() == true)
+													  .toList();
+		
+		for(Recipe item: recipes_vg) {
+			System.out.println(item);
+		}
+		
+		return recipes_vg;
+		
+	}
+	
+	@GetMapping("/vegetarian")
+	public List<Recipe> returnVegetarian() throws IOException{
+		//List<Recipe> recipes = reader.readFile();
+		List<Recipe> recipes_veg = reader.readFile().stream()
+													.filter(item -> item.getVegetarian() == true)
+													.toList();
+		
+		for(Recipe item: recipes_veg) {
+			System.out.println(item);
+		}
+		
+		return recipes_veg;
+		
+	}
+	
+	@GetMapping("/gluten-free")
+	public List<Recipe> returnGlutenFree() throws IOException{
+		//List<Recipe> recipes = reader.readFile();
+		List<Recipe> recipes_Gfree = reader.readFile().stream()
+													.filter(item -> item.getGlutenFree() == true)
+													.toList();
+		
+		for(Recipe item: recipes_Gfree) {
+			System.out.println(item);
+		}
+		
+		return recipes_Gfree;
 		
 	}
 }

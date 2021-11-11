0874827243b1a8065404ee43f4b6c075eecc2641
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2021-11-11 13:02:16 -0600

    assignment 14 update

---
+++ showreviews/src/main/java/com/coderscampus/showreviews/web/DashboardController.java
@@ -0,0 +1,12 @@
+package com.coderscampus.showreviews.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+	@GetMapping("/dashboard")
+	public String getDashBoard() {
+		return "Dashboard";
+	}
+}

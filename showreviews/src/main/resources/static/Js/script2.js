f01b85371e9d6ecbfa423a03e5df8a192097f17f
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2021-12-29 13:00:32 -0600

    trying to show trailers for tv shows
    trying to show trailers for tv shows

--- showreviews/src/main/resources/static/Js/script2.js
+++ showreviews/src/main/resources/static/Js/script2.js
@@ -8,83 +8,71 @@ const searchURL = BASE_URL + '/search/tv?'+API_KEY;
 
 
 const genres = [
-    {
-      "id": 28,
-      "name": "Action"
-    },
-    {
-      "id": 12,
-      "name": "Adventure"
-    },
-    {
-      "id": 16,
-      "name": "Animation"
-    },
-    {
-      "id": 35,
-      "name": "Comedy"
-    },
-    {
-      "id": 80,
-      "name": "Crime"
-    },
-    {
-      "id": 99,
-      "name": "Documentary"
-    },
-    {
-      "id": 18,
-      "name": "Drama"
-    },
-    {
-      "id": 10751,
-      "name": "Family"
-    },
-    {
-      "id": 14,
-      "name": "Fantasy"
-    },
-    {
-      "id": 36,
-      "name": "History"
-    },
-    {
-      "id": 27,
-      "name": "Horror"
-    },
-    {
-      "id": 10402,
-      "name": "Music"
-    },
-    {
-      "id": 9648,
-      "name": "Mystery"
-    },
-    {
-      "id": 10749,
-      "name": "Romance"
-    },
-    {
-      "id": 878,
-      "name": "Science Fiction"
-    },
-    {
-      "id": 10770,
-      "name": "TV Movie"
-    },
-    {
-      "id": 53,
-      "name": "Thriller"
-    },
-    {
-      "id": 10752,
-      "name": "War"
-    },
-    {
-      "id": 37,
-      "name": "Western"
-    }
-  ]
+	{
+	"id":10759,
+	"name":"Action & Adventure"
+	},
+	{
+	"id":16,
+	"name":"Animation"
+	},
+	{
+	"id":35,
+	"name":"Comedy"
+	},
+	{
+	"id":80,
+	"name":"Crime"
+	},
+	{
+	"id":99,
+	"name":"Documentary"
+	},
+	{
+	"id":18,
+	"name":"Drama"
+	},
+	{
+	"id":10751,
+	"name":"Family"
+	},
+	{
+	"id":10762,
+	"name":"Kids"
+	},
+	{
+	"id":9648,
+	"name":"Mystery"
+	},
+	{
+	"id":10763,
+	"name":"News"
+	},
+	{
+	"id":10764,
+	"name":"Reality"
+	},
+	{
+	"id":10765,
+	"name":"Sci-Fi & Fantasy"
+	},
+	{
+	"id":10766,
+	"name":"Soap"
+	},
+	{
+	"id":10767,
+	"name":"Talk"
+	},
+	{
+	"id":10768,
+	"name":"War & Politics"
+	},
+	{
+	"id":37,
+	"name":"Western"
+	}
+ ]
 
 const main = document.getElementById('main');
 const form =  document.getElementById('form');
@@ -160,7 +148,7 @@ function clearBtn(){
         clear.addEventListener('click', () => {
             selectedGenre = [];
             setGenre();            
-            getMovies(API_URL);
+            getTvshows(API_URL);
         })
         tagsEl.append(clear);
     }
@@ -210,9 +198,9 @@ function showTvShows(data) {
     data.forEach(tvshow => {
         const {name, poster_path, vote_average, overview, id} = tvshow;
 		console.log(name);
-        const movieEl = document.createElement('div');
-        movieEl.classList.add('movie');
-        movieEl.innerHTML = `
+        const tvEl = document.createElement('div');
+        tvEl.classList.add('tvShow');
+        tvEl.innerHTML = `
              <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${name}">
 
             <div class="tv-info">
@@ -230,7 +218,7 @@ function showTvShows(data) {
         
         `
 
-        main.appendChild(movieEl);
+        main.appendChild(tvEl);
 
         document.getElementById(id).addEventListener('click', () => {
           console.log(id)
@@ -267,7 +255,7 @@ function openNav(tvshow) {
         })
         
         var content = `
-        <h1 class="no-results">${movie.original_title}</h1>
+        <h1 class="no-results">${tvShow.original_title}</h1>
         <br/>
         
         ${embed.join('')}
